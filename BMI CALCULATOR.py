a117694a3f9d39658e474e8b6b6b284cb6f3919b
Aditya kumar <168696099+Adityakumar9631@users.noreply.github.com>
2024-07-05 07:12:22 +0530

    Add files via upload

---
+++ BMI CALCULATOR.py
@@ -0,0 +1,9 @@
+print("Enter name of candidate ")
+Name= input()
+print("Enter weight of candidate in kg ")
+Weight=float(input())
+print("Enter height of candidate in meter  ")
+Height= float(input())
+Avg= Height*Height
+BMI=Weight/Height
+print("BMI of candidate "," ",BMI)
